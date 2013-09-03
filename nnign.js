3bd7aa46ec502b1aff380ef7939a40f293a7cf04
Adam Derewecki <derewecki@gmail.com>
2013-09-02 22:41:50 -0700

    Scope deletion to body.home selector

--- nnign.js
+++ nnign.js
@@ -1,9 +1,11 @@
 function poll() {
-  var n=document.getElementById('contentArea');
-  if (n) { 
+  var node = document.querySelector('body.home #contentArea');
+  if (node) {
     n.parentNode.removeChild(n);
+    // continue to check for the node, in case it AJAXes back in
     setTimeout(poll, 1000);
   } else {
+    // check aggressively on initial load
     setTimeout(poll, 100);
   }
 }
