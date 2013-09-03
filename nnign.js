2cd7f03e89eff0f534e7743b4f270eeab18026eb
Adam Derewecki <derewecki@gmail.com>
2013-09-02 22:28:25 -0700

    Continue to poll in case #contentArea is rebuilt

--- nnign.js
+++ nnign.js
@@ -2,6 +2,7 @@ function poll() {
   var n=document.getElementById('contentArea');
   if (n) { 
     n.parentNode.removeChild(n);
+    setTimeout(poll, 1000);
   } else {
     setTimeout(poll, 100);
   }
